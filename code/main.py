5399911396ee30f3725eafdc95bb0f5abe2d6f7e
yz3060 <jzqh0110@gmail.com>
2018-02-27 17:48:09 -0500

    backend signup connected with database

--- code/main.py
+++ code/main.py
@@ -1,9 +1,11 @@
 from google.appengine.ext import vendor
 vendor.add('lib')
 
-from flask import Flask
+from flask import Flask, session, request, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 import config
+import db_connect
+
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -25,9 +27,63 @@ def connect_db():
         db.session.close()
         return str(result)
 
+
 @app.route('/')
 def index():
     return "Hello, World (lets see how long a change takes III)!"
+
+
+'''
+The signup page
+'''
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+
+'''
+New user signup
+'''
+@app.route('/newUser', methods=['POST'])
+def newUser():
+	username = str(request.form['username'])
+	email = str(request.form['email'])
+	password = str(request.form['password'])
+	zipcode = str(request.form['zipcode'])
+	res = db_connect.newUser(username, email, password, zipcode)
+	if res: # signup successful
+		session['logged_in'] = True
+		session['username'] = username
+		return redirect('/')		
+	else:
+		return render_template("signup.html", error="User exists!")
+
+
+'''
+The login page
+'''
+@app.route('/login')
+def login():
+	return render_template("login.html")
+
+
+'''
+User login
+'''
+@app.route('/userLogin', methods=['POST'])
+def userLogin():
+	username = str(request.form['username'])
+	password = str(request.form['password'])
+	table = db_connect.userTable()
+	usr = table.find(username, password)
+	if usr:
+		session['logged_in'] = True
+		session['username'] = username
+		return redirect('/')
+	else:
+		return render_template('login.html', error="Invalid username or password")
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
