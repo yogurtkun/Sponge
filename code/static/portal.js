67f16b31d0624d1e5979e67cb5c9f152002c0ac8
yogurtkun <yogurtkun@126.com>
2018-03-21 21:26:58 -0400

    fix a bug when address is null

--- code/static/portal.js
+++ code/static/portal.js
@@ -2,34 +2,33 @@ $(document).ready(() => {
     var userinfo = new Vue({
         el: '#user-info',
         data: {
-            user: { 
-                'username': "Empty", 
-                "email": "Empty", 
-                "zipcode": "Empty", 
-                "phoneNumber": "Empty", 
+            user: {
+                'username': "Empty",
+                "email": "Empty",
+                "zipcode": "Empty",
+                "phoneNumber": "Empty",
                 "address": "Empty"
             },
             myaddress: {
-                ad1: "",
-                ad2: "",
-                city: "",
-                region: "",
-                country: ""
+                "ad1": "",
+                "ad2": "",
+                "city": "",
+                "region": "",
+                "country": ""
             }
         },
         methods: {
-            changeInfo: function(){
+            changeInfo: function () {
                 this.user.address = JSON.stringify(this.myaddress)
-                console.log(this.user)
 
                 $.ajax({
                     url: '/updateUser',
                     type: 'POST',
                     data: this.user,
-                    success: (data)=>{
-                        if(data === "Success"){
+                    success: (data) => {
+                        if (data === "Success") {
                             alert("Save Successfully");
-                        }else{
+                        } else {
                             alert("Save Failed");
                         }
                     }
@@ -42,13 +41,15 @@ $(document).ready(() => {
                 type: 'POST',
                 success: (data) => {
                     retUser = JSON.parse(data)
-                    this.user = Object.assign({},retUser);
-                    let tempaddr = JSON.parse(this.user.address);
+                    this.user = Object.assign({}, retUser);
+                    
+                    if (this.user.address !== null) {
+                        let tempaddr = JSON.parse(this.user.address);
+                        this.myaddress = tempaddr
+                    }
 
-                    this.myaddress = tempaddr
-
-                    for(let k in this.user){
-                        if(this.user[k] === "" || this.user[k] === null ){
+                    for (let k in this.user) {
+                        if (this.user[k] === "" || this.user[k] === null) {
                             this.user[k] = "";
                         }
                     }
@@ -58,112 +59,112 @@ $(document).ready(() => {
     });
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('#buy-post-table').bootstrapTable({
-        url:"/buypostlist",
-        method:'POST',
-        sidePagination:"client",
-        pagination:true,
-        sortName:"postId",
-        search:true,
-        pageSize:10,
+        url: "/buypostlist",
+        method: 'POST',
+        sidePagination: "client",
+        pagination: true,
+        sortName: "postId",
+        search: true,
+        pageSize: 10,
         strictSearch: false,
-        columns:[
+        columns: [
             {
-                title:'Post Id',
-                field:'postId',
-                align:'center',
-                valign:'middle',
-                width:'20%',
-                formatter:function(value,row,index){
-                    return '<a href="/BuyerPost?postId=' + value+'">'+value+'</a>';
+                title: 'Post Id',
+                field: 'postId',
+                align: 'center',
+                valign: 'middle',
+                width: '20%',
+                formatter: function (value, row, index) {
+                    return '<a href="/BuyerPost?postId=' + value + '">' + value + '</a>';
                 }
             },
             {
-                title:'Title',
-                field:'title',
-                align:'center',
-                valign:'middle',
-                width:'20%'
+                title: 'Title',
+                field: 'title',
+                align: 'center',
+                valign: 'middle',
+                width: '20%'
             },
             {
-                title:'Category',
-                field:'category',
-                align:'center',
-                valign:'middle',
-                width:'20%'
+                title: 'Category',
+                field: 'category',
+                align: 'center',
+                valign: 'middle',
+                width: '20%'
             },
             {
-                title:'Price',
-                field:'price',
-                align:'center',
-                valign:'middle',
-                width:'20%',
+                title: 'Price',
+                field: 'price',
+                align: 'center',
+                valign: 'middle',
+                width: '20%',
                 sortable: true
             },
             {
-                title:'Time',
-                field:'time',
-                align:'center',
-                valign:'middle',
-                width:'20%',
+                title: 'Time',
+                field: 'time',
+                align: 'center',
+                valign: 'middle',
+                width: '20%',
                 sortable: true
             }
         ]
     });
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('#sell-post-table').bootstrapTable({
-        url:"/sellpostlist",
-        method:'POST',
-        sidePagination:"client",
-        pagination:true,
-        sortName:"postId",
-        search:true,
-        pageSize:10,
+        url: "/sellpostlist",
+        method: 'POST',
+        sidePagination: "client",
+        pagination: true,
+        sortName: "postId",
+        search: true,
+        pageSize: 10,
         strictSearch: false,
-        columns:[
+        columns: [
             {
-                title:'Post Id',
-                field:'postId',
-                align:'center',
-                valign:'middle',
-                width:'20%',
-                formatter:function(value,row,index){
-                    return '<a href="/SellerPost?postId=' + value+'">'+value+'</a>';
+                title: 'Post Id',
+                field: 'postId',
+                align: 'center',
+                valign: 'middle',
+                width: '20%',
+                formatter: function (value, row, index) {
+                    return '<a href="/SellerPost?postId=' + value + '">' + value + '</a>';
                 }
             },
             {
-                title:'Title',
-                field:'title',
-                align:'center',
-                valign:'middle',
-                width:'20%'
+                title: 'Title',
+                field: 'title',
+                align: 'center',
+                valign: 'middle',
+                width: '20%'
             },
             {
-                title:'Category',
-                field:'category',
-                align:'center',
-                valign:'middle',
-                width:'20%'
+                title: 'Category',
+                field: 'category',
+                align: 'center',
+                valign: 'middle',
+                width: '20%'
             },
             {
-                title:'Price',
-                field:'price',
-                align:'center',
-                valign:'middle',
-                width:'20%',
+                title: 'Price',
+                field: 'price',
+                align: 'center',
+                valign: 'middle',
+                width: '20%',
                 sortable: true
             },
             {
-                title:'Time',
-                field:'time',
-                align:'center',
-                valign:'middle',
-                width:'20%',
+                title: 'Time',
+                field: 'time',
+                align: 'center',
+                valign: 'middle',
+                width: '20%',
                 sortable: true
             }
-        ]        
+        ]
     });
 })
