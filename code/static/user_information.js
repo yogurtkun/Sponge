78bde02515256d84c0e0b6a3c8cab47e8d853a6d
zhichengMLE <jason-dean@outlook.com>
2018-03-27 01:24:46 -0400

    Add user information page

---
+++ code/static/user_information.js
@@ -0,0 +1,54 @@
+
+    var userinfo = new Vue({
+        el: '#user-info',
+        data: {
+            reviews : "null",
+            orders : "null",
+        },
+        created(){
+            var usr_text = $("#user-name").text().replace(/ /g,'');
+            var index = usr_text.indexOf("User")
+            var username = usr_text.substr(index+5, usr_text.length-index-1-5)
+            var tdata = {"username" : username}
+
+            $.ajax({
+            url: '/getReviewsToUser',
+            type: 'POST',
+            data: tdata,
+            success: (data) => {
+                json = JSON.parse(data)
+                console.log(json)
+
+                this.orders = json
+            },
+            }).fail(function($xhr) {
+              var data = $xhr.responseJSON;
+              console.log(data);
+            });
+        },
+
+        methods:{
+            query_review_info: function(){
+                var usr_text = $("#user-name").text().replace(/ /g,'');
+                var index = usr_text.indexOf("User")
+                var username = usr_text.substr(index+5, usr_text.length-index-1-5)
+                var tdata = {"username" : username}
+
+                $.ajax({
+                url: '/getReviewsToUser',
+                type: 'POST',
+                data: tdata,
+                success: (data) => {
+                    json = JSON.parse(data)
+                    console.log(json)
+
+                    this.reviews = json
+                },
+                }).fail(function($xhr) {
+                  var data = $xhr.responseJSON;
+                  console.log(data);
+                });
+            }
+        }
+    });
+
