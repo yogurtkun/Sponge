0b7883c90229c778c1f4fe78212e60faa092aa1e
yz3060 <jzqh0110@gmail.com>
2018-04-02 19:11:00 -0400

    add shipping info. to schema

--- code/schema.py
+++ code/schema.py
@@ -119,7 +119,8 @@ class Order(db.Model):
     transactionType = db.Column(db.String(32))
     senderAddress = db.Column(db.Text)
     receiverAddress = db.Column(db.Text)
-    Carrier  =db.Column(db.Enum())
+    carrier = db.Column(db.Enum("USPS", "FedEX", "UPS", "DHL"))
+    trackNo = db.Column(db.String(64))
 
     def __repr__(self):
         return '<Order %r>' % self.orderId
