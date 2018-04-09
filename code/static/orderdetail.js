cfc5ffb7c6500b75472170d6c23863671333412f
zhichengMLE <jason-dean@outlook.com>
2018-04-09 15:49:03 -0400

    Remove redundent checkbox in order_detail and reload page after review.

--- code/static/orderdetail.js
+++ code/static/orderdetail.js
@@ -54,7 +54,7 @@ var userinfo = new Vue({
             }).fail(function($xhr) {
               var data = $xhr.responseJSON;
             });
-
+            window.location.reload()
         },
 
         ShipOrder:function(){
